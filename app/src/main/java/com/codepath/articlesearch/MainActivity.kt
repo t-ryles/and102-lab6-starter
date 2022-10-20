e226190379b5f131e096c8ce558340efd0d88e83
t-ryles <tajhakeem.ryles@gmail.com>
2022-10-19 19:11:32 -0500

    -Day 2- Finished lab

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -3,12 +3,15 @@ package com.codepath.articlesearch
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.articlesearch.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
@@ -26,11 +29,8 @@ private const val TAG = "MainActivity/"
 //    "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${SEARCH_API_KEY}"
 
 class MainActivity : AppCompatActivity() {
-    //private val articles = mutableListOf<Article>()
-    //private lateinit var articlesRecyclerView: RecyclerView
     private lateinit var binding: ActivityMainBinding
 
-    // invoking replaceFragment method
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -38,53 +38,28 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        // Call helper method to swap the FrameLayout with the fragment
-        replaceFragment(ArticleListFragment())
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        // define your fragments here
+        val bestSellerBooksFragment: Fragment = BestSellerBooksFragment()
+        val articleListFragment: Fragment = ArticleListFragment()
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // handle navigation selection
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.bestsellingIcon -> fragment = bestSellerBooksFragment
+                R.id.articlesIcon -> fragment = articleListFragment
+            }
+            fragmentManager.beginTransaction().replace(R.id.article_frame_layout, fragment).commit()
+            true
+        }
+
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.articlesIcon
     }
-
-
-
-        // Moved the ArticleListFragment
-
-//        articlesRecyclerView = findViewById(R.id.articles)
-//        val articleAdapter = ArticleAdapter(this, articles)
-//        articlesRecyclerView.adapter = articleAdapter
-//        articlesRecyclerView.layoutManager = LinearLayoutManager(this).also {
-//            val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
-//            articlesRecyclerView.addItemDecoration(dividerItemDecoration)
-//        }
-//
-//        val client = AsyncHttpClient()
-//        client.get(ARTICLE_SEARCH_URL, object : JsonHttpResponseHandler() {
-//            override fun onFailure(
-//                statusCode: Int,
-//                headers: Headers?,
-//                response: String?,
-//                throwable: Throwable?
-//            ) {
-//                Log.e(TAG, "Failed to fetch articles: $statusCode")
-//            }
-//
-//            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
-//                Log.i(TAG, "Successfully fetched articles: $json")
-//                try {
-//                    val parsedJson = createJson().decodeFromString(
-//                        SearchNewsResponse.serializer(),
-//                        json.jsonObject.toString()
-//                    )
-//                    parsedJson.response?.docs?.let { list ->
-//                        articles.addAll(list)
-//                        articleAdapter.notifyDataSetChanged()
-//                    }
-//                } catch (e: JSONException) {
-//                    Log.e(TAG, "Exception: $e")
-//                }
-//            }
-//
-//        })
-
-
-    //  helper method within MainActivity to be able to add swap the fragment:
 
     private fun replaceFragment(articleListFragment: ArticleListFragment) {
         val fragmentManager = supportFragmentManager
